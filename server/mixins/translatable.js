6e6832b30a3d66947207a814eef28a7574e0e296
Joona Romppanen <joona.romppanen@gmail.com>
2016-02-06 20:33:06 +0200

    Added simply translation key/value validation pre-save

--- server/mixins/translatable.js
+++ server/mixins/translatable.js
@@ -19,7 +19,42 @@ function populateDefaultValue(properties) {
 	return translations;
 }
 
+function validateLanguageKeys(defaults, translations) {
+	Object.keys(translations).forEach(langKey => {
+		if (!defaults.hasOwnProperty(langKey)) {
+			throw new Error('Invalid translation inserted');
+		}
+	});
+}
+
+function validateTranslationValues(translations) {
+	for (let value in translations) {
+		if (typeof value !== 'string' || !(value instanceof String)) {
+			throw new Error('Invalid translation value: ' + value);
+		}
+	}
+}
+
 module.exports = (Model, options) => {
-	Model.defineProperty('translations', {type: 'object', default: populateDefaultValue(options.properties)});
+	const defaults = populateDefaultValue(options.properties);
+	Model.defineProperty('translations', {type: 'object', default: defaults});
 	// TODO: Before save validate object structure
+	Model.observe('before save', async (ctx, next) => {
+		const translations = ctx.data.translations;
+		if (!translations) {
+			ctx.data.translations = defaults;
+			return next();
+		}
+
+		if (translations) {
+			try {
+				validateLanguageKeys(defaults, translations);
+				validateTranslationValues(translations);
+			} catch (err) {
+				return next(err);
+			}
+		}
+
+		next();
+	});
 };
