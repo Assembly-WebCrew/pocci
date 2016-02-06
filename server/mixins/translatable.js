b834903e535cdd6029ec3427da2440df4acbb47f
Joona Romppanen <joona.romppanen@gmail.com>
2016-02-06 14:21:34 +0200

    ... Rationalise translation object structure

--- server/mixins/translatable.js
+++ server/mixins/translatable.js
@@ -4,13 +4,17 @@ import R from 'ramda'
 if (!config.availableLanguages) throw new Error('Missing required configuration "availableLanguages" from config.json');
 
 module.exports = (Model, options) => {
-	let initialTranslations = {};
-	let languages = config.availableLanguages.reduce((dict, key) => {
-		dict[key] = '';
+	const languages = config.availableLanguages.reduce((dict, key) => {
+		dict[key] = {};
 		return dict;
 	}, {});
+
 	if (options.properties && Array.isArray(options.properties)) {
-		options.properties.forEach(propertyName => initialTranslations[propertyName] = R.clone(languages));
+		options.properties.forEach(translatableProperty => {
+			config.availableLanguages.forEach(langKey => {
+				languages[langKey][translatableProperty] = '';
+			});
+		});
 	}
-	Model.defineProperty('translations', {type: 'object', default: initialTranslations});
+	Model.defineProperty('translations', {type: 'object', default: languages});
 };
